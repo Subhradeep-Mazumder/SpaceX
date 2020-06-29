a5a545013aeaed75a32d2042d87b448f45d97f4e
unknown <subhradeep707@gmail.com>
2020-06-29 16:14:50 +0530

    Updates

---
+++ precache-manifest.e0118aa575c1017be1bf69ddd606aeaf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3c15f4b54d922721541d63a41b6e3a",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "4f3f36032a8db5fc6c10",
+    "url": "/SpaceX/static/css/main.313d8fd1.chunk.css"
+  },
+  {
+    "revision": "e616aec2808520ca449b",
+    "url": "/SpaceX/static/js/2.64cf7f06.chunk.js"
+  },
+  {
+    "revision": "6c99299aa0ccf7cd363681b2638ef8ef",
+    "url": "/SpaceX/static/js/2.64cf7f06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3f36032a8db5fc6c10",
+    "url": "/SpaceX/static/js/main.8734895d.chunk.js"
+  },
+  {
+    "revision": "b159356e875134aa4eda",
+    "url": "/SpaceX/static/js/runtime-main.5a683b47.js"
+  },
+  {
+    "revision": "bb50eaebca5fa2dcf8814139c7377dd7",
+    "url": "/SpaceX/static/media/Big Falcon Rocketmain.bb50eaeb.jpg"
+  },
+  {
+    "revision": "93349224005875e5e4c1d55e2fc567aa",
+    "url": "/SpaceX/static/media/Falcon 1 Rocket.93349224.jpg"
+  },
+  {
+    "revision": "e833714050dbe51d4e815b1edea2f09a",
+    "url": "/SpaceX/static/media/Falcon 9 Rocket.e8337140.jpg"
+  },
+  {
+    "revision": "53c5edf6641f9994cea6764c1f4a9217",
+    "url": "/SpaceX/static/media/Falcon heavy Rocket.53c5edf6.jpg"
+  },
+  {
+    "revision": "01fe21fc1b8ffd4674305c2e430263c8",
+    "url": "/SpaceX/static/media/core.01fe21fc.jpg"
+  },
+  {
+    "revision": "45c94c7acb7e88c472348240155ab6dc",
+    "url": "/SpaceX/static/media/core1.45c94c7a.png"
+  },
+  {
+    "revision": "0e3c8f49e3beb7b6cad9a8282d2609ce",
+    "url": "/SpaceX/static/media/elen-mask.0e3c8f49.jpg"
+  },
+  {
+    "revision": "ae4723f54ae2b5999563b7b52c5e2b45",
+    "url": "/SpaceX/static/media/launches.ae4723f5.jpg"
+  },
+  {
+    "revision": "72792e6f38cc6dadf54afd6aa3adb36d",
+    "url": "/SpaceX/static/media/login background.72792e6f.jpg"
+  },
+  {
+    "revision": "d3cb1f1e230c869cc5942b263bbb9bfc",
+    "url": "/SpaceX/static/media/missons.d3cb1f1e.jpg"
+  },
+  {
+    "revision": "c563b9826c4bd1e7a50083975225cfba",
+    "url": "/SpaceX/static/media/videoplayback.c563b982.mp4"
+  }
+]);
